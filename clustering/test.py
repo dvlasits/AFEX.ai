e6da9fd59c7eaf43646a8216f137344bdbef33c8
mm2504 <mm2504@cam.ac.uk>
2023-06-10 15:46:38 +0100

    Did the scoring

--- clustering/test.py
+++ clustering/test.py
@@ -1,14 +1,16 @@
 from sklearn.cluster import SpectralClustering
 import numpy as np
 
-sc = SpectralClustering(n_clusters=2, affinity='precomputed')
+if __name__ == '__main__':
 
-simil_mat = np.random.rand(5,5)
-simil_mat = ((simil_mat-simil_mat.T)/2)**2
-simil_mat += np.eye(5)
-print(simil_mat)
+    sc = SpectralClustering(n_clusters=2, affinity='precomputed')
+
+    simil_mat = np.random.rand(5,5)
+    simil_mat = ((simil_mat-simil_mat.T)/2)**2
+    simil_mat += np.eye(5)
+    print(simil_mat)
 
 
-clusters = sc.fit_predict(simil_mat)
-print(clusters)
+    clusters = sc.fit_predict(simil_mat)
+    print(clusters)
 
