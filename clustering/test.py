cbc3ff8ed1d5e15376a6f0d39e5dcb3d3b6377f3
oboril <jan.oboril@gmail.com>
2023-06-10 11:57:14 +0100

    added prototype for clustering

--- clustering/test.py
+++ clustering/test.py
@@ -1,7 +1,14 @@
 from sklearn.cluster import SpectralClustering
+import numpy as np
 
-SpectralClustering(n_clusters=8, affinity='precomputed')
+sc = SpectralClustering(n_clusters=2, affinity='precomputed')
 
-simil_mat
+simil_mat = np.random.rand(5,5)
+simil_mat = ((simil_mat-simil_mat.T)/2)**2
+simil_mat += np.eye(5)
+print(simil_mat)
 
+
+clusters = sc.fit_predict(simil_mat)
+print(clusters)
 
