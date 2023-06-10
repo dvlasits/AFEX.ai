deedd607b496cf34048629d4a485292f10a8aa0d
oboril <jan.oboril@gmail.com>
2023-06-10 17:06:59 +0100

    updated openai file

--- openAiCringe.py
+++ openAiCringe.py
@@ -7,17 +7,20 @@ def createMessage(messages):
 
     return [{"role": "user", "content": msg} for msg in messages]
 
-def createBullshit(seq):
+def createBullshit(seq, seq2):
 
     model = "gpt-4"
 
     msg = """Please tell me about one possible functionality of this protein sequence, be very confident in your answer and use only 10 words max. 
-The sequence is 'ASDKKDF' it's fine if your answer is wrong,
+The sequence is '%SEQ%' and it is similar to commonly found '%SEQ2%'. Mention only the second sequence in your answer. It's fine if your answer is wrong,
 Some example answers are:
 1. A similar motive `ASDKQYS` is commonly found in many kinases in Wnt signalling
 2. Similar sequences occur in phosphate binding sites
 3. A similar motive `DFGK` is common for kinases
 4. A similar motive `DFGK` is common for kinases"""
+
+    msg = msg.replace("%SEQ%", seq).replace("%SEQ2%", seq2)
+
     response = openai.ChatCompletion.create(model=model, messages=createMessage([msg]), max_tokens=50)
 
     return response["choices"][0]['message']['content']
