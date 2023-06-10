be0aa055eb81f178def5f23263d2f465a2c6f894
oboril <jan.oboril@gmail.com>
2023-06-10 16:28:31 +0100

    updated website

--- ui/static/predict.js
+++ ui/static/predict.js
@@ -3,23 +3,57 @@ $(document).ready(function () {
 });
 
 function predict() {
-    console.log("predict!");
+    let sendData = { 'input': $("#seq-input").val() };
 
-    let chars = "ASDFGHMFAJDSASDFGHMFAJDSASDFGHMFAJDSASDFGHMFAJDSASDFGHMFAJDSASDFGHMFAJDS";
-    let group = "000111000000000111111100002222222000111110000220000000000000000000000000";
-    let output = $("#result");
-    for (var i = 0; i < chars.length; i++) {
-        let ch = chars.charAt(i);
-        let cl = group.charAt(i);
-        if (cl == '1'){
-            output.append("<span class='green'>" + ch + "</span>");
+    $.ajax({
+        url: '/predict-api',
+        type: "POST",
+        data: JSON.stringify(sendData),
+        contentType: "application/json; charset=utf-8",
+        success: function (recData) {
+            let chars = recData.seq;
+            let group = recData.labels;
+            let output = $("#result");
+            output.empty();
+            for (var i = 0; i < chars.length; i++) {
+                let ch = chars.charAt(i);
+                let cl = group.charAt(i);
+                if (cl == '1') {
+                    output.append("<span class='green'>" + ch + "</span>");
+                }
+                else if (cl == '2') {
+                    output.append("<span class='red'>" + ch + "</span>");
+                }
+                else {
+                    output.append("<span>" + ch + "</span>");
+                }
+
+            }
+
+            let probs_out = $("#pred-func-output");
+            probs_out.empty();
+            let probs = recData.probs;
+            for (var i = 0; i < probs.length; i++) {
+                probs_out.append("<span>" + probs[i][0] +"</span>");
+                probs_out.append("<span>" + probs[i][1] +"</span>");
+            }
+
+            let explanation_out = $("#pred-explanation");
+            explanation_out.empty();
+            let expl = recData.explanation;
+            for (var i = 0; i < expl.length; i++) {
+                if (expl[i][1] == "1") {
+                    explanation_out.append("<span class='green'>" + expl[i][0] +"</span>");
+                }
+                else if (expl[i][1] == "2") {
+                    explanation_out.append("<span class='red'>" + expl[i][0] +"</span>");
+                }
+                else {
+                    explanation_out.append("<span>" + expl[i][0] +"</span>");
+                }
+                
+                explanation_out.append("<span>" + expl[i][2] +"</span>");
+            }
         }
-        else if (cl == '2'){
-            output.append("<span class='red'>" + ch + "</span>");
-        }
-        else {
-            output.append("<span>" + ch + "</span>");
-        }
-        
-    }
+    });
 }
