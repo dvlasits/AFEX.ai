3850f154d397e45d491c9c7c30da7b260646a9fb
mm2504 <mm2504@cam.ac.uk>
2023-06-10 12:29:20 +0100

    Changed alignment

--- alignment.py
+++ alignment.py
@@ -6,23 +6,30 @@ def global_alignment_score(sequence1, sequence2):
     aligner = Align.PairwiseAligner(scoring='blastp')
     return aligner.align(sequence1,sequence2).score
 
-def distance_between_two(df, id1, id2):
-    row1 = df.loc[df['id']==id1]
-    row2 = df.loc[df['id']==id2]
-    sequence1 = row1.at[id1, 'sequence']
-    sequence2 = row2.at[id2, 'sequence']
-    return global_alignment_score(sequence1, sequence2)
+def local_alignment_score(sequence1, sequence2):
+    aligner = Align.PairwiseAligner(scoring='blastp')
+    aligner.mode = 'local'
+    return aligner.align(sequence1,sequence2).score
 
-def distance_matrix(df):
+def distance_between_two(df, id1, id2, distance_function):
+    sequence1 = df.at[id1, 'sequence']
+    sequence2 = df.at[id2, 'sequence']
+    return distance_function(sequence1, sequence2)
+
+def distance_matrix(df, is_global = True):
+    df = df.reset_index()
     n = len(df.index)
     matrix = np.zeros((n,n))
+    distance_function = global_alignment_score
+    if not is_global:
+        distance_function = local_alignment_score
     for i in range(n):
         for j in range(n):
-            matrix[i][j] = distance_between_two(df,i,j)
+            matrix[i][j] = distance_between_two(df,i,j, distance_function)
     return matrix
 
 
 if __name__ == '__main__':
     df = pd.read_csv('data/all_data.csv')
     df = df.head(10)
-    print(distance_matrix(df))
+    print(distance_matrix(df, is_global=False))
