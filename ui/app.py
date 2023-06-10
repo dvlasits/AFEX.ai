75e88668c498443e667479a1e5e80452e83b47b8
oboril <jan.oboril@gmail.com>
2023-06-10 16:44:42 +0100

    deploying ui

--- ui/app.py
+++ ui/app.py
@@ -19,6 +19,7 @@ def predict_api():
   print("Got data", data)
   return {"seq": "ASDFGHMFAJDSASDFGHMFAJDSASDFGHMFAJDSASDFGHMFAJDSASDFGHMFAJDSASDFGHMFAJDS", "labels": "000111000000000111111100002222222000111110000220000000000000000000000000", "probs": [["Signalling", "0.87"], ["Glycolysis", "0.11"], ["Lyase", "0.02"]], "explanation": [["ASDGQTS", "1", "A similar motive `ASDKQYS` is commonly found in many kinases in Wnt signalling"], ["FGHMFAJ", "2", "Similar sequences occur in phosphate binding sites"], ["DFGH", "1", "A similar motive `DFGK` is common for kinases"], ["DFGH", "1", "A similar motive `DFGK` is common for kinases"]]}
 
+#deploy now
 
 if __name__ == "__main__":
   app.run()
